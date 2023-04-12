790a448e1d1c2c29f5a6cb07ad46e5d3c86925d9
John Castrillon <jcastrillon087@gmail.com>
2023-04-12 16:00:46 -0400

    update default apps script with conditional logic for app installations

--- scripts/default-apps.sh
+++ scripts/default-apps.sh
@@ -14,71 +14,119 @@ fi
 # Install Xcode Command Line Tools
 xcode-select --install
 
-# Install Slack
-curl -o slack.dmg https://downloads.slack-edge.com/releases/macos/Slack-latest.dmg
-hdiutil mount slack.dmg
-cp -R "/Volumes/Slack.app" /Applications/
-hdiutil unmount "/Volumes/Slack.app"
-rm slack.dmg
+# Check if Chrome is installed
+if mdfind "kMDItemContentType == com.apple.application-bundle && kMDItemDisplayName == 'Google Chrome.app'" | grep -q "Google Chrome.app"; then
+    echo "Google Chrome is already installed."
+else
+    echo "Installing Google Chrome!"
+    # Download & install Google Chrome
+    curl -o googlechrome.dmg https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+    hdiutil mount googlechrome.dmg
+    sudo /Volumes/Google\ Chrome/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --install
+    sudo mv "/Volumes/Google Chrome/Google Chrome.app" "/Applications/"
+    open -a "Google Chrome" --args --make-default-browser
+    hdiutil unmount /Volumes/Google\ Chrome
+    rm googlechrome.dmg
+fi
 
-# Install Postman
-curl -o postman.zip https://dl.pstmn.io/download/latest/osx
-unzip postman.zip
-rm postman.zip
-mv Postman.app /Applications/
+# Check if Slack is installed
+if [ -d "/Applications/Slack.app" ]; then
+    echo "Slack is already installed."
+else
+    echo "Installing Slack!"
+    # Install Slack
+    curl -o slack.dmg https://downloads.slack-edge.com/releases/macos/Slack-latest.dmg
+    hdiutil mount slack.dmg
+    cp -R "/Volumes/Slack.app" /Applications/
+    hdiutil unmount "/Volumes/Slack.app"
+    rm slack.dmg
+fi
 
-# Install Visual Studio Code
-curl -o vscode.zip https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal
-unzip vscode.zip
-rm vscode.zip
-mv "Visual Studio Code.app" /Applications/
+# Check if VS Code is installed
+if [ -d "/Applications/Visual Studio Code.app" ]; then
+    echo "VS Code is already installed."
+else
+    echo "Installing VS Code!"
+    # Install Visual Studio Code
+    curl -L -o vscode.zip "https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal"
+    unzip vscode.zip
+    rm vscode.zip
+    mv "Visual Studio Code.app" /Applications/
+fi
 
-# Download & install Firefox
-curl -o firefox.dmg "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
-hdiutil mount firefox.dmg
-sudo cp -r "/Volumes/Firefox/Firefox.app" "/Applications/"
-hdiutil unmount "/Volumes/Firefox"
-rm firefox.dmg
+# Check if Postman is installed
+if [ -d "/Applications/Postman.app" ]; then
+    echo "Postman is already installed."
+else
+    echo "Installing Postman!"
+    # Install Postman
+    curl -o postman.zip https://dl.pstmn.io/download/latest/osx
+    unzip postman.zip
+    rm postman.zip
+    mv Postman.app /Applications/
+fi
 
-# Download & install Google Chrome
-curl -o googlechrome.dmg https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
-hdiutil mount googlechrome.dmg
-sudo /Volumes/Google\ Chrome/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --install
-sudo mv "/Volumes/Google Chrome/Google Chrome.app" "/Applications/"
-open -a "Google Chrome" --args --make-default-browser
-hdiutil unmount /Volumes/Google\ Chrome
-rm googlechrome.dmg
+# Check if Brave Browser is installed
+if [ -d "/Applications/Brave Browser.app" ]; then
+    echo "Brave Browser is already installed."
+else
+    echo "Installing Brave Browser!"
+    # Install Brave Browser
+    curl -L -o brave.dmg "https://laptop-updates.brave.com/latest/osx"
+    hdiutil mount brave.dmg
+    cp -R "/Volumes/Brave Browser/Brave Browser.app" /Applications/
+    hdiutil unmount "/Volumes/Brave Browser"
+    rm brave.dmg
+fi
 
-# Install VLC Player
-curl -L -o vlc.dmg "https://get.videolan.org/vlc/3.0.16/macosx/vlc-3.0.16.dmg"
-hdiutil mount vlc.dmg
-cp -R "/Volumes/vlc-3.0.16/VLC.app" /Applications/
-hdiutil unmount "/Volumes/vlc-3.0.16"
-rm vlc.dmg
+# Check if Firefox is installed
+if mdfind "kMDItemContentType == com.apple.application-bundle && kMDItemDisplayName == 'Firefox.app'" | grep -q "Firefox.app"; then
+    echo "Firefox is already installed."
+else
+    echo "Installing Firefox!"
+    # Download & install Firefox
+    curl -o firefox.dmg "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
+    hdiutil mount firefox.dmg
+    sudo cp -r "/Volumes/Firefox/Firefox.app" "/Applications/"
+    hdiutil unmount "/Volumes/Firefox"
+    rm firefox.dmg
+fi
 
-# Install Brave Browser
-curl -L -o brave.dmg "https://laptop-updates.brave.com/latest/osx"
-hdiutil mount brave.dmg
-cp -R "/Volumes/Brave Browser/Brave Browser.app" /Applications/
-hdiutil unmount "/Volumes/Brave Browser"
-rm brave.dmg
+# Check if MongoDB Compass is installed
+if [ -d "/Applications/MongoDB Compass.app" ]; then
+    echo "MongoDB Compass is already installed."
+else
+    echo "Installing MongoDB Compass!"
+    # Install MongoDB Compass
+    curl -L -o mongodb-compass.dmg "https://downloads.mongodb.com/compass/mongodb-compass-1.28.1-macos-x64.dmg"
+    hdiutil mount mongodb-compass.dmg
+    cp -R "/Volumes/MongoDB Compass/MongoDB Compass.app" /Applications/
+    hdiutil unmount "/Volumes/MongoDB Compass"
+    rm mongodb-compass.dmg
+fi
 
-# Install Visual Studio Code
-curl -L -o vscode.zip "https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal"
-unzip vscode.zip
-rm vscode.zip
-mv "Visual Studio Code.app" /Applications/
+# Check if VLC Player is installed
+if [ -d "/Applications/VLC.app" ]; then
+    echo "VLC Player is already installed."
+else
+    echo "Installing VLC Player!"
+    # Install VLC Player
+    curl -L -o vlc.dmg "https://get.videolan.org/vlc/3.0.16/macosx/vlc-3.0.16.dmg"
+    hdiutil mount vlc.dmg
+    cp -R "/Volumes/vlc-3.0.16/VLC.app" /Applications/
+    hdiutil unmount "/Volumes/vlc-3.0.16"
+    rm vlc.dmg
+fi
 
-# Install Discord
-curl -L -o discord.dmg "https://discord.com/api/download?platform=osx"
-hdiutil mount discord.dmg
-cp -R "/Volumes/Discord/Discord.app" /Applications/
-hdiutil unmount "/Volumes/Discord"
-rm discord.dmg
-
-# Install MongoDB Compass
-curl -L -o mongodb-compass.dmg "https://downloads.mongodb.com/compass/mongodb-compass-1.28.1-macos-x64.dmg"
-hdiutil mount mongodb-compass.dmg
-cp -R "/Volumes/MongoDB Compass/MongoDB Compass.app" /Applications/
-hdiutil unmount "/Volumes/MongoDB Compass"
-rm mongodb-compass.dmg
+# Check if Discord is installed
+if [ -d "/Applications/Discord.app" ]; then
+    echo "Discord is already installed."
+else
+    echo "Installing Discord!"
+    # Install Discord
+    curl -L -o discord.dmg "https://discord.com/api/download?platform=osx"
+    hdiutil mount discord.dmg
+    cp -R "/Volumes/Discord/Discord.app" /Applications/
+    hdiutil unmount "/Volumes/Discord"
+    rm discord.dmg
+fi

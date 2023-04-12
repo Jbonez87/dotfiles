39a289808091f70dc0b7111b13d7b1646cfd8517
John Castrillon <jcastrillon087@gmail.com>
2023-04-11 22:30:06 -0400

    add windows-setup.bat script

---
+++ windows-setup.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo Installing Chocolatey...
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+echo Installing NVM...
+choco install nvm -y
+
+echo Installing Node.js...
+call nvm install 16.14.2
+
+echo Installing Rust...
+choco install rust -y
+
+echo Installation complete.

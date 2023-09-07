229fc043ab6a0f1acd72611e896687ea19024902
John Castrillon <jcastrillon087@gmail.com>
2023-09-07 08:12:23 -0700

    fix default apps script directory variable

--- scripts/mac/mac-setup.sh
+++ scripts/mac/mac-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 SCRIPTDIR=`cd "$(dirname "$0")" && pwd`
-DEFAULT_APPS_SCRIPT="./default-apps.sh"
+DEFAULT_APPS_SCRIPT="$SCRIPTDIR/default-apps.sh"
 
 echo "Run app defaults script first? (Please answer Yes or No)"
 select script_choice in yes no; do
